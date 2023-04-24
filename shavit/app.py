9ceb627d049ab498bf350d381f0688542a0e3e69
yarinnetzer <netzer.yarin@gmail.com>
2023-04-24 13:25:29 +0300

    start shavit project

---
+++ shavit/app.py
@@ -0,0 +1,13 @@
+# Import flask module
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def index():
+    return 'Hello to Flask!'
+ 
+# main driver function
+if __name__ == "__main__":
+    app.run()
+
